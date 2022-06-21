01b90c8b47076955c680a786776fb9b872692443
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-21 19:02:35 +0530

    SPREAD

---
+++ reastOperators.js
@@ -0,0 +1,13 @@
+// Spread Operators
+// In Array
+let numbers = [1, 2, 3];
+let newNumbers = [...numbers, 4];
+console.log(newNumbers);
+
+// In Objects
+
+let person = {
+  name: "Sandeep",
+};
+let newObject = { ...person, age: 32 };
+console.log(newObject);
