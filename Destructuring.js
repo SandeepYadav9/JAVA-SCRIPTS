283036bce3bd64056eeaaca6b71eeaca421faf7b
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-22 16:15:47 +0530

    Update Destructuring.js

--- Destructuring.js
+++ Destructuring.js
@@ -6,7 +6,22 @@ const number=[1,2,3,4];
 [num1, ,num3]=number;
 console.log(num1, num3)
 
-// Diffenitions are in **README.md**
+**
+   ** Destructer **
+  Note - Object and array are Reference Types 
+  
+     Easly estract array element or object propertes and store them into variables
+     
+     1. Array Destructer
+     // Example. 
+      [s, p]=["Sandeep", "Sunil"];
+      console.log(s, p)// Sandeep, Sunil
+      
+      2. Object Destructer 
+      Example.
+      {name}={name:"Monu"}
+       console.log(name) // Monu
+**
 
 
 
