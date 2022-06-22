39da060de8eec8f88e2bd2b3680b10972fd17d52
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-23 00:19:42 +0530

    Create Arrow-function.js

---
+++ Arrow-function.js
@@ -0,0 +1,24 @@
+Arrow Functions
+
+1. Normal Function
+   -Normal function start with function key word and function name and function body
+   function funName(){
+     ///
+   }
+
+
+2. Arrow Functions  Syntax are :   ()=>{}
+    -Store  inside const  than right side equal sign than arrow function Syntax Parentheses () that hold list of arguments  and equal sign & greater => than than function body 
+  const funName=()=>{ // }
+  
+  
+  const funName =(name, age)=>{  
+    console.log(name, age)
+  }
+  
+  funName("Piyu", 21) // Piyu, 21 
+
+    const funName =(name)=>{  
+    console.log(name)
+  }
+  funName() // undefiend
