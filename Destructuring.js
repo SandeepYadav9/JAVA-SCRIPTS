352d685ca4d7aa93aceea6fe3600a1f74cbe2f2f
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-22 16:02:30 +0530

    Update Destructuring.js

--- Destructuring.js
+++ Destructuring.js
@@ -1,10 +1,12 @@
 const numbers=[1,2,3,4];
-[num1, num2]=numbers;
+[num1, num2]=numbers; // Primitive Type
 console.log(num1, num2)
 
 const number=[1,2,3,4];
 [num1, ,num3]=number;
 console.log(num1, num3)
+
+// Diffenitions are in **README.md**
 
 
 
