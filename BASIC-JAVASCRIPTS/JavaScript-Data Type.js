7778db759987d1059b3b6c67dd1cc8680ca56c92
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-29 16:55:38 +0530

    Delete JavaScript-Data Type.js

--- BASIC-JAVASCRIPTS/JavaScript-Data Type.js
+++
@@ -1,3 +0,0 @@
-1. You have to store the name of your hometown in a variable name hometown.
-    Also, you have to declare and initialise a variable named doYouStay
-      which stores either true or false.
