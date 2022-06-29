ef97a19263077507affa2d6378d56684938ad2f6
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-29 11:51:31 +0530

    Update SpreadOperators.js

--- SpreadOperators.js
+++ SpreadOperators.js
@@ -1,5 +1,10 @@
 // Spread Operators
 // In Array
+Spead & Rest Operators **Spread and rest ** are three dot ...
+
+//Spread - Used to split up Array elements and Object Properties
+
+
 let numbers = [1, 2, 3];
 let newNumbers = [...numbers, 4];
 console.log(newNumbers);
